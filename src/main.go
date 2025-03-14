fa69e9596f51b7897c8cbf62e624c78e9cc8401a
Andreas Krath <andreas.krath+github@gmail.com>
2025-03-14 11:06:12 +0100

    Feature: Change index route and embed HTML page
    HTML is currently empty, but will contain something in the future.

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"net/http"
 
@@ -8,16 +9,21 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+//go:embed static/index.html
+var Index string
 func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
-	router.Get("/*", index)
+	router.Get("/", index)
+	router.Get("/index", index)
 
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", router)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Under construction..."))
+	w.Write([]byte(Index))
+	return
+}
 	return
 }
