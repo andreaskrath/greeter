f7241a5cb97f87e0496918f9accfbc7494221e47
Andreas Krath <andreas.krath+github@gmail.com>
2025-03-14 11:07:08 +0100

    Feature: Add `/fact` endpoint for getting a random fact

--- src/main.go
+++ src/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -11,11 +13,15 @@ import (
 
 //go:embed static/index.html
 var Index string
+
+var Seed = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/", index)
 	router.Get("/index", index)
+	router.Get("/fact", fact)
 
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", router)
@@ -23,6 +29,14 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(Index))
+	return
+}
+
+func fact(w http.ResponseWriter, r *http.Request) {
+	randomIndex := Seed.Intn(len(Facts))
+	fact := Facts[randomIndex]
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fact))
 	return
 }
 	return
